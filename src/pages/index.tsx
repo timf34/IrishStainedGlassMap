8d2492f8e2571f9a5d6d038ae71a20ce5c90a7a0
timf34 <farrelti@tcd.ie>
2023-07-09 01:00:36 +0100

    improved homepage styling slightly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,12 @@ import { NextPage } from 'next';
 
 const Home: NextPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-4xl font-bold mb-4">Locations</h1>
-            <div className="w-full max-w-2xl h-96 rounded-md shadow-md overflow-hidden">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 relative">
+            <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold mb-4 z-10">Irish Stained Glass Map</h1>
+            <div className="flex items-center justify-center w-full max-w-5xl my-8 h-[80vh] rounded-md shadow-xl overflow-hidden">
                 <MapComponent locations={locations} />
             </div>
+            <footer className="text-xl z-15"><a href="https://timfarrelly.com/">Created by Tim</a></footer>
         </div>
     );
 };
