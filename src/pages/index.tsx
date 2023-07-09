6e3e1bcbf01ee629c16ba93ebbba71b1f8a97e74
timf34 <farrelti@tcd.ie>
2023-07-09 01:06:29 +0100

    improved homepage styling slightly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,8 @@ import { NextPage } from 'next';
 
 const Home: NextPage = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 relative">
-            <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-bold mb-4 z-10">Irish Stained Glass Map</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 px-4 sm:px-8 lg:px-16 relative">
+            <h1 className="text-3xl md:text-4xl lg:text-4xl xl:text-5xl font-bold mb-4 z-10">Irish Stained Glass Map</h1>
             <div className="flex items-center justify-center w-full max-w-5xl my-8 h-[80vh] rounded-md shadow-xl overflow-hidden">
                 <MapComponent locations={locations} />
             </div>
